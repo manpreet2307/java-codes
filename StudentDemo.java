dfb4903d02854443ec48ce9879e8edb38920136d
manpreet2307 <preetman3691@gmail.com>
2026-05-18 22:04:16 +0530

    student demo

---
+++ StudentDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Student {
+    String name; int age;
+    void input(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter name: ");
+        name=sc.nextLine();
+        System.out.print("Enter age: ");
+        age=sc.nextInt();
+    }
+    void display(){System.out.println("Name: "+name+", Age: "+age);}
+}
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.input(); s.display();
+    }
+}
