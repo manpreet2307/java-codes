28cfdd4d751cd2f26ad5894402af2bc7d5323e52
manpreet2307 <preetman3691@gmail.com>
2026-05-18 22:15:21 +0530

    sum naturl

---
+++ SumNatural.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SumNatural {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n=sc.nextInt(), sum=0;
+        for(int i=1;i<=n;i++) sum+=i;
+        System.out.println("Sum = " + sum);
+    }
+}
