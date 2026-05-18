e80565e37453ab99782d5a160d045854f860f9c0
manpreet2307 <preetman3691@gmail.com>
2026-05-18 22:03:12 +0530

    null check

---
+++ NullCheck.java
@@ -0,0 +1,6 @@
+public class NullCheck {
+    public static void main(String[] args) {
+        String s=null;
+        if(s==null) System.out.println("String is null");
+        else System.out.println(s.length());
+    }}
