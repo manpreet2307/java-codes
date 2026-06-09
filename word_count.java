5f71d97fcbca51f2f4166a67928783cdb8709ac1
manpreet2307 <preetman3691@gmail.com>
2026-06-10 01:09:27 +0530

    word count

---
+++ word_count.java
@@ -0,0 +1,8 @@
+public class word_count {
+    public static void main(String[] args){
+        String str = "I love Java";
+
+        String words[] = str.split(" ");
+        System.out.println(words.length);
+    }
+}
